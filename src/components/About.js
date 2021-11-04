2fbea56ac1e5f66e4cce51503d1e5b620c8b0341
isotop786 <marufislam77@gmail.com>
2021-11-04 23:46:27 +0600

    abuot update

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,7 @@ const About = ()=>{
         <p>Simple React App for finding Github Users</p>
         <p>Version: 1.0.0</p>
         <p>Developer: Maruf </p>
+        <p>Contact at  <a href="mailto:marufislam77@gmail.com">marufislam77@gmail.com</a></p>
         </>
     )
 }
