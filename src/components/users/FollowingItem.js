771137ffb2d8b228c080b54c4cd86444b088016b
isotop786 <marufislam77@gmail.com>
2021-11-14 18:25:35 +0600

    following fix

--- src/components/users/FollowingItem.js
+++ src/components/users/FollowingItem.js
@@ -5,7 +5,8 @@ const FollowingItem = ({following})=>{
         <img src={following.avatar_url} alt="" style={{width:"60px"}} className="round-img"/>
         <h3>{following.login}</h3>
         <div>
-            <a href={following.html_url}className="btn btn-sm btn-dark my-1">Profile</a>
+       
+            <a  href={`/user/${following.login}`} className="btn btn-sm btn-dark my-1">Profile</a>
         </div>
     </div>
     )
